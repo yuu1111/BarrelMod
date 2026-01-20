9c0455ec90b71c3da08904bf07d3313349a5cd98
yuu111 <yuu210yuu@gmail.com>
2026-01-21 01:17:49 +0900

    Fix item/block structure based on Hytale modding docs
    - Revert to lowercase item ID (barrelmod_barrel)
    - Use server.lang for translations with server. prefix
    - Remove Common/ prefix from texture and icon paths
    - Use single Textures object with Top/Bottom/Sides
    - Add Hytale module dependencies to manifest
    - Remove items.lang files (use server.lang instead)

--- src/main/java/com/github/yuu1111/barrelmod/commands/BarrelCommand.java
+++ src/main/java/com/github/yuu1111/barrelmod/commands/BarrelCommand.java
@@ -4,13 +4,7 @@ import com.github.yuu1111.barrelmod.BarrelModPlugin;
 
 import com.hypixel.hytale.server.core.command.system.AbstractCommand;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
-import com.hypixel.hytale.server.core.command.system.arguments.system.DefaultArg;
-import com.hypixel.hytale.server.core.command.system.arguments.system.RequiredArg;
-import com.hypixel.hytale.server.core.command.system.arguments.types.ArgTypes;
 import com.hypixel.hytale.server.core.Message;
-import com.hypixel.hytale.server.core.universe.PlayerRef;
-import com.hypixel.hytale.server.core.inventory.ItemStack;
-
 import java.util.concurrent.CompletableFuture;
 
 public class BarrelCommand extends AbstractCommand {
@@ -21,7 +15,6 @@ public class BarrelCommand extends AbstractCommand {
         super("barrel", "Barrel mod commands");
         this.plugin = plugin;
 
-        addSubCommand(new GiveSubCommand(plugin));
         addSubCommand(new InfoSubCommand(plugin));
         addSubCommand(new SaveSubCommand(plugin));
     }
@@ -34,39 +27,8 @@ public class BarrelCommand extends AbstractCommand {
 
     private void sendHelp(CommandContext context) {
         context.sendMessage(Message.raw("=== Barrel Mod Commands ==="));
-        context.sendMessage(Message.raw("/barrel give <player> [amount] - Give barrels to a player"));
         context.sendMessage(Message.raw("/barrel info - Show mod information"));
         context.sendMessage(Message.raw("/barrel save - Save barrel data"));
-    }
-
-    private static class GiveSubCommand extends AbstractCommand {
-        private static final String BARREL_ITEM_ID = "BarrelMod_Barrel";
-        private final BarrelModPlugin plugin;
-        private final RequiredArg<PlayerRef> playerArg;
-        private final DefaultArg<Integer> amountArg;
-
-        public GiveSubCommand(BarrelModPlugin plugin) {
-            super("give", "Give barrels to a player");
-            this.plugin = plugin;
-            this.playerArg = withRequiredArg("player", "Target player", ArgTypes.PLAYER_REF);
-            this.amountArg = withDefaultArg("amount", "Number of barrels", ArgTypes.INTEGER, 1, "1");
-        }
-
-        @Override
-        protected CompletableFuture<Void> execute(CommandContext context) {
-            PlayerRef playerRef = context.get(playerArg);
-            int amount = context.get(amountArg);
-            amount = Math.max(1, Math.min(amount, 64));
-
-            if (playerRef == null) {
-                context.sendMessage(Message.raw("Player not found"));
-                return CompletableFuture.completedFuture(null);
-            }
-
-            context.sendMessage(Message.raw("Use: /give " + BARREL_ITEM_ID + " " + amount));
-            context.sendMessage(Message.raw("to give " + amount + " barrel(s) to " + playerRef.getUsername()));
-            return CompletableFuture.completedFuture(null);
-        }
     }
 
     private static class InfoSubCommand extends AbstractCommand {
